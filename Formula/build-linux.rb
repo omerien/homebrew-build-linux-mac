42954c6df899346e85e6e2cbe7f8574ce4b804ab
omerien <53191414+omerien@users.noreply.github.com>
2022-04-13 20:21:42 +0200

    Create build-linux.rb

---
+++ Formula/build-linux.rb
@@ -0,0 +1,11 @@
+class buildLinux < Formula
+  desc "Main formulae that brings include files and a patched linux version. DO NOT INSTALL WiP"
+  homepage "https://github.com/omerien/build-linux"
+  url ""
+  sha256 ""
+
+  depends_on "omerien/build-linux-mac/elf-header"
+
+  def install
+  end
+end
