b19536ceac06ac68606518b3425657433e931390
omerien <53191414+omerien@users.noreply.github.com>
2022-04-14 18:40:49 +0200

    Dependancies

--- Formula/build-linux.rb
+++ Formula/build-linux.rb
@@ -5,6 +5,12 @@ class buildLinux < Formula
   sha256 ""
 
   depends_on "omerien/build-linux-mac/elf-header"
+  depends_on "findutils"
+  depends_on "coreutils"
+  depends_on "gnu-tar" 
+  depends_on "gnu-sed" 
+  depends_on "make"
+  depends_on "ncurses"
 
   def install
   end
