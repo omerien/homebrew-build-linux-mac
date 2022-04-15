3fb6f0e5999cbce4ad9a5f04875a00bdb48f1fe0
omerien <53191414+omerien@users.noreply.github.com>
2022-04-15 20:45:11 +0200

    Update build-linux.rb

--- Formula/build-linux.rb
+++ Formula/build-linux.rb
@@ -14,21 +14,28 @@ class BuildLinux < Formula
 
   def install
     include.install "endian.h"
-    include.install "stdio.h" # Because the one of MacOS SDK doesn't work
     include.install "wordswap.h"
     include.install "byteswap.h"
     include.install "features.h"
     include.install "libio.h"
     include.install "stdarg.h"
     include.install "stddef.h"
+    include.install "stdlib.h" ### Because the one of MacOS SDK doesn't work
+    include.install "stdio.h" # Because the one of MacOS SDK doesn't work
     include.install "wchar.h"
     include.install "_G_config.h"
     include.install "gnu/stubs.h"
     include.install "gnu/stubs-32.h"
     include.install "gnu/stubs-64.h"
     include.install "sys/cdefs.h"
+    include.install "sys/_types.h" ##
+    include.install "machine/_types.h" ##
     include.install "bits/endian.h"
     include.install "bits/predefs.h"
+    include.install "bits/stdio.h"
+    include.install "bits/stdio_lim.h"
+    include.install "bits/stdlib.h" ##
+    include.install "bits/sys_errlist.h"
     include.install "bits/wordsize.h"
     include.install "bits/wchar.h"
     include.install "bits/byteswap.h"
