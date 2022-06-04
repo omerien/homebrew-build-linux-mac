57bc9ebd9cbf07164ae8a79a5a14414d6a48b0c6
omerien <53191414+omerien@users.noreply.github.com>
2022-06-04 12:24:11 +0200

    Update elf-header.rb

--- Formula/elf-header.rb
+++ Formula/elf-header.rb
@@ -2,7 +2,7 @@ class ElfHeader < Formula
   desc "An elf.h header for building Linux kernel on macOS. Came from nsabovic/homebrew-public"
   homepage "https://github.com/omerien/build-linux"
   url "https://github.com/omerien/build-linux/releases/download/do-not-download/elf.h.1.tar.gz"
-  sha256 "810b604a6643aa0840f0fceaaa8814525ede13c26de8c10bb1d346ad0e724f69"
+  sha256 "d6fbf43f5ce70c74bbbf7dcf2da17102a7545a86c225e859aea743af94c8f340"
 
   depends_on "libelf" 
   # Using local clone because libelf might disappear since it is outdated
