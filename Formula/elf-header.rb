25bd32a337a8727133d2571e55e89f59b7b6a3e7
omerien <53191414+omerien@users.noreply.github.com>
2022-06-04 12:21:34 +0200

    Update elf-header.rb

--- Formula/elf-header.rb
+++ Formula/elf-header.rb
@@ -4,7 +4,7 @@ class ElfHeader < Formula
   url "https://github.com/omerien/build-linux/releases/download/do-not-download/elf.h.1.tar.gz"
   sha256 "810b604a6643aa0840f0fceaaa8814525ede13c26de8c10bb1d346ad0e724f69"
 
-  depends_on "omerien/build-linux-mac/libelf-local" 
+  depends_on "libelf" 
   # Using local clone because libelf might disappear since it is outdated
 
   def install
