7d7d30212e7d060c820ee29bb971e08aa5acc204
omerien <53191414+omerien@users.noreply.github.com>
2022-06-04 12:09:17 +0200

    Update build-linux.rb

--- Formula/build-linux.rb
+++ Formula/build-linux.rb
@@ -3,6 +3,7 @@ class BuildLinux < Formula
   homepage "https://github.com/omerien/build-linux-mac"
   url "https://github.com/omerien/homebrew-build-linux-mac/releases/download/do-not-download-1/Sample.zip"
   sha256 "305fb609e8c92293cfd10cd0e48c3c9b78c3eb015efc8940cc849dca9f298bdd"
+  version 1
 
   depends_on "omerien/build-linux-mac/elf-header"
   depends_on "findutils"
